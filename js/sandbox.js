e0b6608492d09d73e6a7c08231e238d278de9395
Daana <126dpelaezayala@frhsd.com>
2024-10-18 13:36:15 -0400

    Logical operators (32 minutes)

--- js/sandbox.js
+++ js/sandbox.js
@@ -40,14 +40,15 @@
 // } while (i<5);
 
 
-// else if statements 
+// logical operators
+
 
 const password = 'p@ss';
 
-if(password.length >=12){
+if(password.length >=12 && password.includes('@')){
     console.log ('that password is mighty strong');
 
-} else if (password.length >=8){
+} else if (password.length >=8 || password.includes('@') && password.length > 5){
     console.log ('that password is long enough!');
 } else {
     console.log ('password is not long enough');
